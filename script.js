b34981934e09a864ab609e2b5c2d2a6d869378b7
Acsilaa <galeengames@gmail.com>
2024-10-07 11:51:19 +0200

    asd

--- script.js
+++ script.js
@@ -2,35 +2,66 @@ let inputs = document.getElementsByTagName('input');
 let btn = document.getElementById("subm");
 
 btn.addEventListener("click", click);
-
+for(let i = 0; i < inputs.length; i++){
+    inputs[i].addEventListener("input", function(){
+        inputs[i].parentElement.classList.remove("error");
+    });
+}
 function click(){
-    //get values
-    let day = inputs[0].value *1;
-    let month = inputs[1].value *1;
-    let year = inputs[2].value *1;
-    //validate values
+
+    let day = inputs[0].value;
+    let month = inputs[1].value;
+    let year = inputs[2].value;
+
     let now = new Date();
-    errors = [];
+    let errors = [];
 
     const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
 
-    if(month < 1 || month > 12 || month == null){
+    if(month < 1 || month > 12 || month == ""){
         errors.push({"month" : "invalid"});
     }
-    if(year < 0 || year == null){
+    if(year < 0 || year == ""){
         errors.push({"year" : "invalid"});
     }
-    if(day < 0 || day > daysInMonth(year, month) || day == null){
+    if(day < 1 || day > daysInMonth(year*1, month*1) || day == ""){
         errors.push({"day" : "invalid"});
     }
     if(errors.length != 0){
         for(let i = 0; i < errors.length; i++){
             let field = Object.keys(errors[i])[0];
             document.getElementById(field).classList.add("error");
+            document.getElementById(field).children[2].innerText = errors[i][field];
         }
         return;
     }
-    //if error send answer
-    //if ok calculate
+    let toCheck = new Date(year,month*1-1, day*1);
+    toCheck.setFullYear(year)
+    errors = [];
+    if(toCheck > now){
+        errors.push({"day":"must be past value"})
+        errors.push({"month":"must be past value"})
+        errors.push({"year":"must be past value"})
+    }
+    if(errors.length != 0){
+        for(let i = 0; i < errors.length; i++){
+            let field = Object.keys(errors[i])[0];
+            document.getElementById(field).classList.add("error");
+            document.getElementById(field).children[2].innerText = errors[i][field];
+        }
+        return;
+    }
+
     //display
+    let diff = now - toCheck
+    
+    years = diff / (1000*60*60*24*365)
+    diff = years - Math.floor(years);
+    years = Math.floor(years);
+    months = diff / (1/12)
+    diff = months - Math.floor(months);
+    months = Math.floor(months);
+    days = Math.floor(diff / (1/30))
+
+    
 }
